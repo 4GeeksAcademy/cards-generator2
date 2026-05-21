78b9c34e3e626f8130b9946c1dfc567ba4d85b64
cmg070490-bit <cmg070490@gmail.com>
2026-05-21 09:43:50 +0000

    Actualizacion 1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import "bootstrap";
 import "./style.css";
 
+window.onload = function () {
+  const simbolos1 = ["♠", "♥", "♦", "♣"];
+  const numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const randomSimbolo = simbolos1[Math.floor(Math.random() * simbolos1.length)];
+  const randomNumero = numeros[Math.floor(Math.random() * numeros.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const simbolos1El = document.getElementById("simbolos1");
+  const simbolos2El = document.getElementById("simbolos2");
+  const numerosEl = document.getElementById("numeros");
+
+  // Pintar valores
+  simbolos1El.innerHTML = randomSimbolo;
+  simbolos2El.innerHTML = randomSimbolo;
+  numerosEl.innerHTML = randomNumero;
+
+  // Color con Bootstrap
+  const isRed = randomSimbolo === "♥" || randomSimbolo === "♦";
+
+  simbolos1El.classList.toggle("text-danger", isRed);
+  simbolos2El.classList.toggle("text-danger", isRed);
 };
